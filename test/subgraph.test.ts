1e3e26938e878965eab903d2b219e4486047b293
a_decimal <akshay@decimal.at>
2023-09-16 00:20:17 +0400

    Add Subgraph and Builder

--- test/subgraph.test.ts
+++ test/subgraph.test.ts
@@ -1,7 +1,7 @@
 import { subgraph } from "../src";
 import { expect } from "chai";
 
-describe("Subgrapgh Testing", function () {
+describe("Subgrapgh Testing", function() {
   it("Fetch Via HTTP", async () => {
     const result = await subgraph.queryData(subgraph.queries.getJobs(1, 0));
     expect(result).to.have.key("data");
@@ -9,7 +9,7 @@ describe("Subgrapgh Testing", function () {
   });
 
   it("Fetch Jobs Structured Data", async () => {
-    const result = await subgraph.getJobs(5, 0);
+    const result = await subgraph.getJobs(1, 0);
     expect(result.length).to.eq(1);
   });
 
